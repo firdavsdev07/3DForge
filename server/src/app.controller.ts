8cc9d204483f8840bb9d3acb56c6b750dc5df758
firdavsdev07 <108564450+firdavsdev07@users.noreply.github.com>
2025-10-19 15:43:40 +0500

    setup server (nest)

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor() {}
+  
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): { message: string } {
+    return { message: 'Welcome to 3DForge Server!' };
   }
 }
